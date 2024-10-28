9106e36be2118c5908d02eb95217452645132c4b
Armindo Maurits <armindo.maurits@zaptec.com>
2024-10-28 07:19:51 +0100

    Started CharactersService and Controller

---
+++ MovieCharactersApi/Services/CharactersService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieCharactersApi.Data;
+using MovieCharactersApi.Data.Entities;
+
+namespace MovieCharactersApi.Services
+{
+    public class CharactersService
+    {
+        private readonly DatabaseContext _context;
+
+        public CharactersService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Character>> GetCharacters()
+        {
+            return await _context.Characters.ToListAsync();
+        }
+
+        public async Task<Character?> GetCharacter(int id)
+        {
+            return await _context.Characters.FindAsync(id);
+        }
+
+        public async Task<Character> CreateCharacter(Character character)
+        {
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+
+            return character;
+        }
+
+        public async Task<bool> DeleteCharacter(int id)
+        {
+            var character = await _context.Characters.FindAsync(id);
+            if (character == null)
+            {
+                return false;
+            }
+
+            _context.Characters.Remove(character);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        private bool CharacterExists(int id)
+        {
+            return _context.Characters.Any(e => e.Id == id);
+        }
+    }
+}
