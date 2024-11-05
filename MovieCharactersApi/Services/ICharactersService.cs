ef35b82ac77e27e67e40da27de9c40ff83fb955a
Armindo Maurits <armindo.maurits@zaptec.com>
2024-11-05 08:43:23 +0100

    doc: updated interface docs

--- MovieCharactersApi/Services/ICharactersService.cs
+++ MovieCharactersApi/Services/ICharactersService.cs
@@ -4,10 +4,38 @@ namespace MovieCharactersApi.Services
 {
     public interface ICharactersService
     {
+        /// <summary>
+        /// Creates a character
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
         Task<Character> CreateCharacter(Character character);
+
+        /// <summary>
+        /// Deleted a character
+        /// </summary>
+        /// <param name="id">ID of the character</param>
+        /// <returns>True if deleted, false otherwise</returns>
         Task<bool> DeleteCharacter(int id);
+
+        /// <summary>
+        /// Gets a character by given ID
+        /// </summary>
+        /// <param name="id">ID of the character</param>
+        /// <returns>Character if found, null otherwise.</returns>
         Task<Character?> GetCharacter(int id);
+
+        /// <summary>
+        /// Gets all characters from the database.
+        /// </summary>
+        /// <returns>List of characters</returns>
         Task<IEnumerable<Character>> GetCharacters();
+
+        /// <summary>
+        /// Updates a given character
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns>Character if updated, null otherwise</returns>
         Task<Character?> UpdateCharacter(Character character);
     }
 }
--- MovieCharactersApi/Services/IMoviesService.cs
+++ MovieCharactersApi/Services/IMoviesService.cs
@@ -4,11 +4,46 @@ namespace MovieCharactersApi.Services
 {
     public interface IMoviesService
     {
+        /// <summary>
+        /// Creates a new movie if the franchise exists.
+        /// </summary>
+        /// <param name="movie">Movie to be created.</param>
+        /// <returns>Movie if created,</returns>
         Task<Movie?> CreateMovie(Movie movie);
+
+        /// <summary>
+        /// Deletes a movie
+        /// </summary>
+        /// <param name="id">ID of the movie</param>
+        /// <returns>true if deleted, false otherwise</returns>
         Task<bool> DeleteMovie(int id);
+
+        /// <summary>
+        /// Gets a movie by given ID
+        /// </summary>
+        /// <param name="id">ID of the movie</param>
+        /// <returns>Movie if found, null otherwise</returns>
         Task<Movie?> GetMovie(int id);
+
+        /// <summary>
+        /// Gets all movies from the database
+        /// </summary>
+        /// <returns>List of movies</returns>
         Task<IEnumerable<Movie>> GetMovies();
+
+        /// <summary>
+        /// Updates the characters in a given movie
+        /// </summary>
+        /// <param name="id">Movie ID</param>
+        /// <param name="characterIds">List of character IDs</param>
+        /// <returns></returns>
         Task<bool> UpdateCharactersInMovie(int id, IEnumerable<int> characterIds);
+
+        /// <summary>
+        /// Updated a given movie   
+        /// </summary>
+        /// <param name="movie">Movie to be updated.</param>
+        /// <returns>Movie that was updated or null if it was not found.</returns>
         Task<Movie?> UpdateMovie(Movie movie);
     }
 }
